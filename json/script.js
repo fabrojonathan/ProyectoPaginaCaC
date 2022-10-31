30740382c5c70a51f0b296735dcd62c2fc5e314f
Jonathan Fabro <fabrojonathan395@gmail.com>
2022-10-30 21:59:49 -0300

    Comprar Tickets JS

--- json/script.js
+++ json/script.js
@@ -6,41 +6,119 @@ const descJunior = 15;
 let nombre = document.getElementById('nombre');
 let apellido = document.getElementById('apellido');
 let correo = document.getElementById('correo');
-let cantidad = document.getElementById('cantidad');
+let cantidadEntradas = document.getElementById('cantidad');
 let categoria = document.getElementById('categoria');
-let pError = document.getElementById('messageError');
-let importeTotal = document.getElementById('importeNeto');
+let errorCampoVacio = document.getElementById('messageError');
+let errorEmailInvalido = document.getElementById('messageErrorEmail');
+let errorCantidad = document.getElementById('messageErrorCantidad');
+let errorCategoria = document.getElementById('messageErrorCategoria');
+let importeText = document.getElementById('importeNeto');
 
 var btnResumen = document.getElementById('btnResumen');
 btnResumen.addEventListener('click', function(){
-    validarCampos(nombre);
-    validarCampos(apellido);
-    validarCampos(correo);
-    validarCampos(cantidad);
-    importeTotal.value = calcularImporteFinal();
-});
+    calcularValorCompra();
+})
 
 var btnBorrar = document.getElementById('btnBorrar');
 btnBorrar.addEventListener('click', function(){
-    limpiarCampos();})
+    limpiarCampos();
+})
 
-function validarCampos(idAtributo){
-    if(idAtributo.value === ""){
-        idAtributo.classList.add("is-invalid");
-        idAtributo.style.borderColor= "red";
-        pError.style.display = "flex";
-        //idAtributo.focus();
+function calcularValorCompra(){
+
+    if(nombre.value === ""){
+        alert("Por favor escriba su nombre!");
+        nombre.focus();
+        nombre.classList.add("is-invalid");
+        mostrarMessageError(errorCampoVacio);
+        return;
     }
-    else{
-        pError.style.display = "none";
-        idAtributo.style.borderColor= "unset";
+
+    if(apellido.value === ""){
+        alert("Por favor escriba su apellido!");
+        apellido.focus();
+        apellido.classList.add("is-invalid");
+        mostrarMessageError(errorCampoVacio);
+        return;
     }
+
+    if(correo.value === ""){
+        alert("Por favor escriba su correo!");
+        correo.focus();
+        correo.classList.add("is-invalid");
+        mostrarMessageError(errorCampoVacio);
+        return;
+    }
+
+    let correoRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+    if(!correoRegex.test(correo.value)){
+        alert('Email invalido! Reintente');
+        correo.classList.add("is-invalid");
+        correo.focus();
+        mostrarMessageError(errorEmailInvalido);
+        return;
+    }
+
+    ocultarMessageError(errorEmailInvalido);
+
+    if(cantidad.value === "" || cantidad.value === '0'){
+        alert('Debe ingresar una cantidad valida!');
+        correo.classList.add("is-invalid");
+        correo.focus();
+        mostrarMessageError(errorCantidad);
+        return;
+    }
+
+    ocultarMessageError(errorCampoVacio);
+    ocultarMessageError(errorCantidad);
+
+    if(categoria.value === ""){
+        alert('Debe seleccionar una categoria!');
+        correo.classList.add("is-invalid");
+        correo.focus();
+        mostrarMessageError(errorCategoria);
+        return;
+    }
+
+    ocultarMessageError(errorCategoria);
+    calcularImporteFinal(categoria);
+    
+}
+
+function mostrarMessageError(message){
+    message.style.display = "flex";
+}
+
+function ocultarMessageError(message){
+    message.style.display = "none";
 }
 
 function calcularImporteFinal(categoria){
+    let importeTotal = (cantidadEntradas.value) * valorEntrada;
+
     switch(categoria.value){
-        case 1:
-            return ((cantidad * valorEntrada) * descEstudiante)/100;
+        case '0':
+            importeText.innerHTML = importeTotal;
+        break;
+
+        case '1':
+            importeTotal -= (descEstudiante / 100 * importeTotal);
+            importeText.innerHTML = importeTotal;
+        break;
+
+        case '2':
+            importeTotal -= (descTrainee / 100 * importeTotal);
+            importeText.innerHTML = importeTotal;
+        break;
+
+        case '3':
+            importeTotal -= (descJunior / 100 * importeTotal);
+            importeText.innerHTML = importeTotal;
+        break;
+
+        default:
+            importeText.innerHTML = '0,00'
         break;
     }
 }
@@ -52,8 +130,9 @@ function limpiarCampos(){
     apellido.style.borderColor= "initial";
     correo.value = "";
     correo.style.borderColor= "initial";
-    cantidad.value = "";
-    cantidad.style.borderColor= "initial";
+    cantidadEntradas.value = "";
+    cantidadEntradas.style.borderColor= "initial";
     categoria.value = 0;
     categoria.style.borderColor= "initial";
+    errorCampoVacio.style.display = "none";
 }
